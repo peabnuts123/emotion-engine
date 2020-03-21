caf138b78539630f5b438c5545c374e893efc0a9
peabnuts123 <peabnuts123@gmail.com>
2020-03-22 10:23:28 +1300

    Add SVG buttons and code tidy up

--- www/js/script.js
+++ www/js/script.js
@@ -1,6 +1,13 @@
 // CONFIG
-// The max number of items to display in the background
-var MAX_ITEMS = 200;
+// The max number of items to have in memory
+var MAX_ITEMS = 2000;
+/** Properties to adorn the button SVG elements with */
+var BUTTON_PROPERTIES = [
+  { id: 'button-sad', class: 'button emotion-sad', labelId: 'label-sad', },
+  { id: 'button-afraid', class: 'button emotion-afraid', labelId: 'label-afraid', },
+  { id: 'button-stressed', class: 'button emotion-stressed', labelId: 'label-stressed', },
+  { id: 'button-determined', class: 'button emotion-determined', labelId: 'label-determined', },
+];
 
 // jQuery utility - only begin processing after page has finished loading
 $(function () {
@@ -30,19 +37,6 @@ $(function () {
     // Convert a JSON string into an actual object
     var data = JSON.parse(payload.data);
 
-    // If this is the first message then remove the "is-loading"
-    //  class from all the buttons
-    if (!hasReceivedFirstMessage) {
-      $('#button-determined').removeClass('is-loading');
-      $('#button-afraid').removeClass('is-loading');
-      $('#button-sad').removeClass('is-loading');
-      $('#button-stressed').removeClass('is-loading');
-
-      // Mark first message as received so we don't
-      //  do this again
-      hasReceivedFirstMessage = true;
-    }
-
     // If there is state information in the data, store that
     // and update the totals
     if (data.state) {
@@ -66,19 +60,17 @@ $(function () {
   // Draw all triangles to the screen (with or without fill)
   // and tell the browser to do this whenever the window size
   // changes as well
-  setupTriangles();
+  setupSVG();
   $(window).on('resize', function () {
-    setupTriangles();
+    setupSVG();
   });
 
   /**
    * Read the server's state object and update button totals based on that
    */
   function refreshTotals() {
-    $('#button-determined').text("I'm feeling determined (" + serverState.determinedCount + ")");
-    $('#button-afraid').text("I'm feeling afraid (" + serverState.determinedCount + ")");
-    $('#button-sad').text("I'm feeling sad (" + serverState.determinedCount + ")");
-    $('#button-stressed').text("I'm feeling stressed (" + serverState.determinedCount + ")");
+    // @TODO this function does nothing
+    // Remove it or give it purpose
   }
 
   /**
@@ -86,13 +78,21 @@ $(function () {
    * We initialise every triangle on the page without any fill color
    * and then call `updateTriangles` to set the fill colors
    * based on the emotions we've received from the server.
+   *
+   * Also initialise SVG shapes for the buttons.
    */
-  function setupTriangles() {
+  function setupSVG() {
     // Responsive triangle size based on window width (in pixels)
     // 0-1000: 50px
     // 1000-2000: 100px
     // etc.
     var TRIANGLE_SIZE_PIXELS = Math.ceil($(window).width() / 1000) * 50;
+    // Response font size based on window width (in pixels)
+    // 0-1000: 20px
+    // 1000-2000: 40px
+    // etc.
+    var labelFontSize = Math.ceil($(window).width() / 1000) * 20 + 'px';
+    $('.button-label').css('font-size', labelFontSize);
 
     // Work out the height of an equilateral triangle
     // This is just Pythagoras' theorem
@@ -102,58 +102,143 @@ $(function () {
     var numTrianglesPerRow = Math.ceil($(window).width() / (TRIANGLE_SIZE_PIXELS / 2)) + 1;
     var numRows = Math.ceil($(window).height() / triangleHeight);
 
+    // Last button row is 2 rows from the last row
+    var lastButtonRowIndex = numRows - 3;
+    // First button row is 4 rows before the last button row
+    var firstButtonRowIndex = lastButtonRowIndex - 4;
+    // Margin is either:
+    //  - 2 triangles on either side
+    //  - 20% of the screen on either side
+    // which ever is greater (almost always the 20% option)
+    var buttonsMargin = Math.max(2, Math.floor(numTrianglesPerRow * 0.20));
+    // The width of each button is the number of UPRIGHT triangles that aren't margin
+    var buttonsWidth = (numTrianglesPerRow - (buttonsMargin * 2)) / 2 * TRIANGLE_SIZE_PIXELS;
+
     // Clear the svg out first
-    $('#console').empty();
+    $('#triangle-container>#triangles').empty();
+    $('#triangle-container>#buttons').empty();
 
     // Toggle this for every second triangle and reset it
     //  for the start of every row
-    var currentTriangleIsUpsideDown = false
+    var currentTriangleIsUpsideDown = false;
 
     // Create an SVG path element for each triangle on the screen
     // Create triangles row-by-row
     for (var y = 0; y < numRows; y++) {
       for (var x = 0; x < numTrianglesPerRow; x++) {
-        // Calculate the three points of a triangle
-        // Note that "top" might actually be bottom if the triangle is upside down
-        var pointTop, pointLeft, pointRight;
 
-        if (currentTriangleIsUpsideDown) {
-          // Calculate the points of an upside down triangle
-          pointTop = {
-            x: (x * TRIANGLE_SIZE_PIXELS / 2),
-            y: y * triangleHeight + triangleHeight,
-          };
-          pointLeft = {
-            x: pointTop.x - (TRIANGLE_SIZE_PIXELS / 2),
-            y: pointTop.y - triangleHeight,
-          };
-          pointRight = {
-            x: pointTop.x + TRIANGLE_SIZE_PIXELS / 2,
-            y: pointTop.y - triangleHeight,
-          };
+        // Check if this coordinate is within the button region
+        // Draw a special shape for regions that are buttons
+        if (y > firstButtonRowIndex && y <= lastButtonRowIndex && x >= buttonsMargin && x < (numTrianglesPerRow - buttonsMargin)) {
+          // Check x again - this is just to make sure we only draw ONE button
+          //  even though the check above will be true for EVERY CELL that the button
+          //  occupies. i.e. if the buttons are 20 triangles wide - we don't
+          //  want to draw the button 20 times - just the first time.
+          if (x === buttonsMargin) {
+            // Points for constructing a parallelogram SVG path
+            var pointTopLeft, pointBottomLeft, pointBottomRight, pointTopRight;
+
+            // The order matters here (unlike in a triangle - where you can draw the three
+            //  points in any order and it will still make a valid shape). So we need to
+            //  make sure the points match their description by checking if the "triangle"
+            //  is upside-down
+            if (currentTriangleIsUpsideDown) {
+              // For an upside-down triangle, "bottom left" will be what was "pointTop"
+              pointBottomLeft = {
+                x: (x * TRIANGLE_SIZE_PIXELS / 2),
+                y: y * triangleHeight + triangleHeight,
+              };
+
+              // "Top left" will be what was "pointLeft" for an upside-down triangle
+              pointTopLeft = {
+                x: pointBottomLeft.x - (TRIANGLE_SIZE_PIXELS / 2),
+                y: pointBottomLeft.y - triangleHeight,
+              };
+            } else {
+              // For a right-side-up triangle, "top left" will be what was "pointTop"
+              pointTopLeft = {
+                x: (x * TRIANGLE_SIZE_PIXELS / 2),
+                y: y * triangleHeight,
+              };
+
+              // "bottom left" will what was "pointLeft" for a right-side up triangle
+              pointBottomLeft = {
+                x: pointTopLeft.x - TRIANGLE_SIZE_PIXELS / 2,
+                y: pointTopLeft.y + triangleHeight,
+              };
+            }
+
+            // We can compute "bottom right" and "top right" just by adding the width of
+            // the button to "top left" and "top right"
+            pointBottomRight = {
+              x: pointBottomLeft.x + buttonsWidth,
+              y: pointBottomLeft.y,
+            };
+            pointTopRight = {
+              x: pointTopLeft.x + buttonsWidth,
+              y: pointTopLeft.y,
+            };
+
+            // Index of which button this is - we will need this for looking up its label, id, etc.
+            var buttonIndex = y - firstButtonRowIndex - 1;
+
+            // Add the button's SVG code
+            $('#triangle-container>#buttons').append('<path ' +
+              'd="M ' + pointTopLeft.x + ' ' + pointTopLeft.y + ' L ' + pointBottomLeft.x + ' ' + pointBottomLeft.y + ' L ' + pointBottomRight.x + ' ' + pointBottomRight.y + ' L ' + pointTopRight.x + ' ' + pointTopRight.y + ' z" ' +
+              'id="' + BUTTON_PROPERTIES[buttonIndex].id + '" ' +
+              'class="' + BUTTON_PROPERTIES[buttonIndex].class + '" ' +
+              '></path>');
+
+            $('#' + BUTTON_PROPERTIES[buttonIndex].labelId).css('top', pointTopLeft.y);
+            $('#' + BUTTON_PROPERTIES[buttonIndex].labelId).css('left', (pointTopLeft.x + pointBottomLeft.x) / 2);
+            $('#' + BUTTON_PROPERTIES[buttonIndex].labelId).css('width', buttonsWidth);
+            $('#' + BUTTON_PROPERTIES[buttonIndex].labelId).css('height', triangleHeight);
+          }
         } else {
-          // Calculate the points of a right-way-up triangle
-          pointTop = {
-            x: (x * TRIANGLE_SIZE_PIXELS / 2),
-            y: y * triangleHeight,
-          };
-          pointLeft = {
-            x: pointTop.x - TRIANGLE_SIZE_PIXELS / 2,
-            y: pointTop.y + triangleHeight,
-          };
-          pointRight = {
-            x: pointTop.x + TRIANGLE_SIZE_PIXELS / 2,
-            y: pointTop.y + triangleHeight,
-          };
+          // Draw triangles
+
+          // Calculate the three points of a triangle
+          // Note that "top" might actually be bottom if the triangle is upside down
+          var pointTop, pointLeft, pointRight;
+
+          if (currentTriangleIsUpsideDown) {
+            // Calculate the points of an upside down triangle
+            pointTop = {
+              x: (x * TRIANGLE_SIZE_PIXELS / 2),
+              y: y * triangleHeight + triangleHeight,
+            };
+            pointLeft = {
+              x: pointTop.x - (TRIANGLE_SIZE_PIXELS / 2),
+              y: pointTop.y - triangleHeight,
+            };
+            pointRight = {
+              x: pointTop.x + TRIANGLE_SIZE_PIXELS / 2,
+              y: pointTop.y - triangleHeight,
+            };
+          } else {
+            // Calculate the points of a right-way-up triangle
+            pointTop = {
+              x: (x * TRIANGLE_SIZE_PIXELS / 2),
+              y: y * triangleHeight,
+            };
+            pointLeft = {
+              x: pointTop.x - TRIANGLE_SIZE_PIXELS / 2,
+              y: pointTop.y + triangleHeight,
+            };
+            pointRight = {
+              x: pointTop.x + TRIANGLE_SIZE_PIXELS / 2,
+              y: pointTop.y + triangleHeight,
+            };
+          }
+
+          // Add the triangle's SVG code (with no fill color set)
+          $('#triangle-container>#triangles').append('<path ' +
+            'd="M ' + pointTop.x + ' ' + pointTop.y + ' L ' + pointLeft.x + ' ' + pointLeft.y + ' L ' + pointRight.x + ' ' + pointRight.y + ' z" ' +
+            '/>');
+
+          // Next triangle will be the other way up
+          currentTriangleIsUpsideDown = !currentTriangleIsUpsideDown;
         }
-
-        // Add the triangle's SVG code (with no fill color set)
-        $('#console').append('<path ' +
-          'd="M ' + pointTop.x + ' ' + pointTop.y + ' L ' + pointLeft.x + ' ' + pointLeft.y + ' L ' + pointRight.x + ' ' + pointRight.y + ' z" ' +
-          '/>');
-
-        // Next triangle will be the other way up
-        currentTriangleIsUpsideDown = !currentTriangleIsUpsideDown;
       }
 
       // The first triangle of the row will be upside down for odd-numbered rows
@@ -165,6 +250,9 @@ $(function () {
 
     // Set all the triangles fill colors
     updateTriangleColors();
+
+    // Register click handlers on the new button elements
+    registerClickHandlers();
   }
 
   /**
@@ -172,7 +260,7 @@ $(function () {
    * emotions we've received from the server
    */
   function updateTriangleColors() {
-    var $paths = $('#console path');
+    var $paths = $('#triangle-container>#triangles path');
     // There may be more paths on the screen or more emotions in the emotion set
     //  so iterate through whatever is the smaller of the two.
     // If we iterated too many then we would get errors trying to either
@@ -192,7 +280,7 @@ $(function () {
    * @param {string} emotion An emotion name e.g. "grateful"
    */
   function addTriangle(emotion) {
-    // We're just adding it to the `emotionSet` array
+    // We're just adding the value of `emotion` to the `emotionSet` array
     //  but we're also making sure we have no more than `MAX_ITEMS`
     //  things in the array.
 
@@ -208,16 +296,18 @@ $(function () {
   }
 
   // Button click handlers
-  $('#button-determined').click(function () {
-    ws.send(JSON.stringify({ emotion: 'determined' }));
-  });
-  $('#button-afraid').click(function () {
-    ws.send(JSON.stringify({ emotion: 'afraid' }));
-  });
-  $('#button-sad').click(function () {
-    ws.send(JSON.stringify({ emotion: 'sad' }));
-  });
-  $('#button-stressed').click(function () {
-    ws.send(JSON.stringify({ emotion: 'stressed' }));
-  });
+  function registerClickHandlers() {
+    $('#button-determined').click(function () {
+      ws.send(JSON.stringify({ emotion: 'determined' }));
+    });
+    $('#button-afraid').click(function () {
+      ws.send(JSON.stringify({ emotion: 'afraid' }));
+    });
+    $('#button-sad').click(function () {
+      ws.send(JSON.stringify({ emotion: 'sad' }));
+    });
+    $('#button-stressed').click(function () {
+      ws.send(JSON.stringify({ emotion: 'stressed' }));
+    });
+  }
 });
